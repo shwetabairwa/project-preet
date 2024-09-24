2f1e71ff17e9ba69f450b72c4316cd383cf343c7
shweta bairwa <shwetabairwa027@gmail.com>
2024-09-24 19:16:15 +0530

    name updated

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/vite-react-deploy/", // YOUR REPO NAME HERE
+  base: "/project-preet/", // YOUR REPO NAME HERE
 })
